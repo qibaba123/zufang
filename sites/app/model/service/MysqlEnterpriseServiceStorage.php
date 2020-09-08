cdb3296032cdfadbcad93e233f71c3b93b194784
yasuo <1581206083@qq.com>
2020-09-08 20:58:28 +0800

    zufang

--- sites/app/model/service/MysqlEnterpriseServiceStorage.php
+++ sites/app/model/service/MysqlEnterpriseServiceStorage.php
@@ -7,7 +7,7 @@ class App_Model_Service_MysqlEnterpriseServiceStorage extends Libs_Mvc_Model_Bas
     public function __construct()
     {
         parent::__construct();
-        $this->_table = 'applet_enterprise_service';
+        $this->_table = 'enterprise_service';
         $this->_pk = 'es_id';
         $this->_shopId = 'es_s_id';
         $this->_df     = 'es_deleted';
