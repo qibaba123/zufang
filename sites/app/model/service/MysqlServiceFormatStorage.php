c2c7d4917aa28a648ba6bf3afd731617f5b0adde
zhangqi <zhangqi@163.com>
2020-09-16 14:26:47 +0800

    zz

--- sites/app/model/service/MysqlServiceFormatStorage.php
+++ sites/app/model/service/MysqlServiceFormatStorage.php
@@ -7,7 +7,7 @@ class App_Model_Service_MysqlServiceFormatStorage extends Libs_Mvc_Model_BaseMod
     public function __construct()
     {
         parent::__construct();
-        $this->_table = 'applet_service_format';
+        $this->_table = 'service_format';
         $this->_pk = 'sf_id';
         $this->_shopId = 'sf_s_id';
         $this->_df     = 'sf_deleted';
