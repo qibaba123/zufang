713af8e3821fc57734193c76bae6cbaf3ae6f37a
yasuo <1581206083@qq.com>
2020-09-20 12:35:52 +0800

    zufang

--- sites/app/controller/applet/ServiceController.php
+++ sites/app/controller/applet/ServiceController.php
@@ -23,7 +23,6 @@ class App_Controller_Applet_ServiceController extends App_Controller_Applet_Init
         $data   = array(
             'id'    => $row['es_id'],
             'name'  => $row['es_name'],
-            'cover' => $this->dealImagePath($row['es_cover']),
             'brief' => $row['es_brief'],
             'price' => $row['es_price'],
             'content' => plum_parse_img_path($row['es_content']),
